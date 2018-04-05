d366a36d013be9c1055981b578846e726d9d0173
Anthony Fuller <amp153@yahoo.com>
2018-04-04 19:08:51 -0500

    changed urls.py
    had redundant code

--- mysite/forum/urls.py
+++ mysite/forum/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('accounts/login/$', login),
-    path('accounts/logout/$', logout),
+    path('accounts/login', login),
+    path('accounts/logout', logout),
 ]
