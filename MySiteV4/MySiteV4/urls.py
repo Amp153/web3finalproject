6d2f4e19dd80fda096052bcae9a9e93256ac7e0f
Anthony Fuller <amp153@yahoo.com>
2018-04-29 13:35:45 -0500

    Remade site to work around bug
    I was getting 404 errors and coudln't find out how to deal with it. Following a tutorial I have completely remade the site. Alot of the code looks similar to the tutorial but I have made changes to it.

---
+++ MySiteV4/MySiteV4/urls.py
@@ -0,0 +1,31 @@
+"""MySiteV4 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth.views import login, logout
+from accounts import views as accounts_views
+from boards import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup', accounts_views.signup, name="signup"),
+    path('boards/<int:pk>', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply', views.reply_topic, name='reply_topic'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+]
